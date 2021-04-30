a5fac50092cb1f07ab1d31dedc9a9af9ef96de34
EugenM <eugen.massini@gmail.com>
2021-04-30 21:33:56 +0200

    fixed types

--- funchain/funchain.py
+++ funchain/funchain.py
@@ -6,15 +6,15 @@ class Chain(collections.Iterator):
     def __init__(self, start_it: Iterator):
         self.it = start_it
 
-    def filter(self, fun) -> 'Pype':
+    def filter(self, fun) -> 'Chain':
         self.it = filter(fun, self.it)
         return self
 
-    def map(self, fun) -> 'Pype':
+    def map(self, fun) -> 'Chain':
         self.it = map(fun, self.it)
         return self
 
-    def map_by(self, fun) -> 'Pype':
+    def map_by(self, fun) -> 'Chain':
         self.it = fun(self.it)
         return self
 
